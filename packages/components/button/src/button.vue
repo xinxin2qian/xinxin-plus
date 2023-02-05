400ec99fad94339509fd1d292ea154c42f12d9ab
xinxinqian <2656853927@qq.com>
2023-02-05 16:41:32 +0800

    feat(button): script

--- packages/components/button/src/button.vue
+++ packages/components/button/src/button.vue
@@ -3,23 +3,34 @@
         <slot>{{ p.size }} || {{ p.type }}</slot>
     </button>
 </template>
-<script lang="ts">
-import { computed, defineComponent } from 'vue';
+<script lang="ts" setup>
+import { computed } from 'vue';
 import { buttonProps } from './button';
-export default defineComponent({
-    name: "XinButton",
-    props: buttonProps,
-    setup(props) {
-        const p = computed(() => {
-            const { size = "meduim", type = "text" } = props;
-            return {
-                size,
-                type
-            }
-        })
-        return {
-            p
-        }
+defineOptions({
+  name: 'XinButton'
+})
+const props = defineProps(buttonProps);
+const p = computed(() => {
+    const { size = "meduim", type = "text" } = props;
+    return {
+        size,
+        type
     }
 })
+// export default defineComponent({
+//     name: "XinButton",
+//     props: buttonProps,
+//     setup(props) {
+//         const p = computed(() => {
+//             const { size = "meduim", type = "text" } = props;
+//             return {
+//                 size,
+//                 type
+//             }
+//         })
+//         return {
+//             p
+//         }
+//     }
+// })
 </script>
