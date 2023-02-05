834bc11b86ff770cfcdd58fdd9d38191f04f6b45
xinxinqian <2656853927@qq.com>
2023-02-05 17:19:49 +0800

    setup script语法糖添加name属性的另一种解决方案

--- example/vite.config.js
+++ example/vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import DefineOptions from "unplugin-vue-define-options/vite"
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'; //用于给setup script组建增加name属性
+// import DefineOptions from "unplugin-vue-define-options/vite"
 export default defineConfig({
-  plugins: [vue(), DefineOptions()],
+  // plugins: [vue(), DefineOptions()],
+  plugins: [
+    vue(), 
+    VueSetupExtend()
+  ]
 })
