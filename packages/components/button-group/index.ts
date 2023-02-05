a25adf2782730c0accab5897ae94ed4d6c6bf63f
xinxinqian <2656853927@qq.com>
2023-02-05 21:44:13 +0800

    feat(button-group): 新增button-group组件

---
+++ packages/components/button-group/index.ts
@@ -0,0 +1,6 @@
+import buttonGroup from './src/button-group.vue';
+import { withInstall } from "@xinxin-plus/utils";
+
+export const XinButtonGroup = withInstall(buttonGroup);
+
+export default XinButtonGroup;
---
+++ packages/components/button-group/src/button-group.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="br-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: "XinButtonGroup"
+})
+</script>
