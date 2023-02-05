673abe1b810f324007e3768c8ebc178bf17c170f
xinxinqian <2656853927@qq.com>
2023-02-05 16:42:38 +0800

    feat(button): 完善button组件，添加props和ts类型定义

--- example/App.vue
+++ example/App.vue
@@ -1,4 +1,4 @@
 <template>
   <h1>我是开发调试页面</h1>
-  <xin-button type="info" size="small"></xin-button>
+  <xin-button type="info" size="small">我是button组件</xin-button>
 </template>
--- packages/components/button/src/button.ts
+++ packages/components/button/src/button.ts
@@ -1,19 +1,61 @@
-import { ExtractPropTypes } from "vue";
-export const ButtonType = ["primary", "success", "info", "warning", "danger", "text"];
-export const ButtonSize = ["large", "medium", "small", "mini"];
+import { ExtractPropTypes, PropType } from "vue";
+import type { ButtonNativeType, ButtonSizeType, ButtonType } from "./interface";
+import { buttonTypes, buttonNativeTypes } from "./interface";
+import { componentSizes } from "@xinxin-plus/constants";
+// export const ButtonType = ["primary", "success", "info", "warning", "danger", "text"];
+// export const ButtonSize = ["large", "medium", "small", "mini"];
 export const buttonProps = {
     type: {
-        type: String,
-        validator(value: string) {
-            return ButtonType.includes(value)
+        type: String as PropType<ButtonType>,
+        default: (): ButtonType => "default",
+        validator(value: ButtonType) {
+            return buttonTypes.includes(value)
         }
     },
     size: {
+        type: String as PropType<ButtonSizeType>,
+        validator(value: ButtonSizeType) {
+            return componentSizes.includes(value)
+        }
+    },
+    plain: {
+        type: Boolean,
+        default: (): Boolean => false
+    },
+    round: {
+        type: Boolean,
+        default: (): Boolean => false
+    },
+    circle: {
+        type: Boolean,
+        default: (): Boolean => false
+    },
+    loading: {
+        type: Boolean,
+        default: (): Boolean => false
+    },
+    disabled: {
+        type: Boolean,
+        default: (): Boolean => false
+    },
+    icon: {
         type: String,
-        validator(value: string) {
-            return ButtonSize.includes(value)
+        default: (): String => ""
+    },
+    autoFocus: {
+        type: Boolean,
+        default: (): Boolean => false
+    },
+    nativeType: {
+        type: String as PropType<ButtonNativeType>,
+        default: (): ButtonNativeType => "button",
+        validator(value: ButtonNativeType) {
+            return buttonNativeTypes.includes(value)
         }
     }
+}
+export const Emits = {
+    click: (evt: MouseEvent): MouseEvent => evt
 }
 // vue自带的ExtractPropTypes进行包装，导出的才能是真正的类型
 export type ButtonProps = ExtractPropTypes<typeof buttonProps>
