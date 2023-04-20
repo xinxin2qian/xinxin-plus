f30f3e5264c181fe3749663a1b525b5c5e106343
xinxinqian <2656853927@qq.com>
2023-04-20 22:14:31 +0800

    build(components): 调整配置，使打包后的结构和我们开发的结构一致

--- packages/components/vite.config.ts
+++ packages/components/vite.config.ts
@@ -9,19 +9,41 @@ export default defineConfig({
     rollupOptions: {
       //忽略打包vue文件
       external: ['vue'],
-      //input: ["index.ts"],
-      output: {
-        globals: {
-          vue: 'Vue'
+      input: ['index.ts'],
+      output: [
+        {
+          // globals: {
+          //   vue: 'Vue'
+          // },
+          // dir: 'dist'
+          //打包格式
+          format: 'es',
+          //打包后文件名
+          entryFileNames: '[name].mjs',
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: 'named',
+          //配置打包根目录
+          dir: '../xinxin-plus/es'
         },
-        dir: 'dist'
-      }
+        {
+          //打包格式
+          format: 'cjs',
+          //打包后文件名
+          entryFileNames: '[name].js',
+          //让打包目录和我们目录对应
+          preserveModules: true,
+          exports: 'named',
+          //配置打包根目录
+          dir: '../xinxin-plus/lib'
+        }
+      ]
     },
     lib: {
       entry: './index.ts',
-      name: 'xinxin-plus',
-      fileName: 'xinxin-plus',
-      formats: ['es', 'umd', 'cjs']
+      name: 'xinxin-plus'
+      // fileName: 'xinxin-plus',
+      // formats: ['es', 'umd', 'cjs']
     }
   },
   plugins: [vue()]
